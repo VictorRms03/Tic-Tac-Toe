75424c17c30b081b1b51253a855f835663d4fcf8
Victor Ramos <victorrms03@gmail.com>
2025-07-23 11:12:49 -0300

    fix: navbar para telas menores

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,14 +7,14 @@ export default function Navbar() {
 
     return (
 
-        <header className="w-full px-6 py-4 shadow-lg sticky top-0 z-50">
-            <div className="max-w-11/12 xl:max-w-9/12 mx-auto flex items-center justify-between">
+        <header className="w-full px-2 md:px-6 py-4 shadow-lg top-0 z-50">
+            <div className="md:max-w-11/12 xl:max-w-9/12 mx-auto flex items-center justify-between">
 
-                <h1 className='text-4xl'>Tic-Tac-Toe: Online</h1>
+                <h1 className='hidden md:inline text-xl md:text-2xl lg:text-4xl'>Tic-Tac-Toe: Online</h1>
         
-                <div className='lg:flex space-x-10'>
+                <div className='flex space-x-2 md:space-x-6 lg:space-x-10'>
 
-                    <button onClick={createGame} className="group bg-black px-4 py-2 rounded hover:bg-white border-2 border-black hover:border-black transition flex items-center gap-2">
+                    <button onClick={createGame} className="group bg-black px-4 py-2 rounded hover:bg-white border-2 border-black hover:border-black transition flex items-center">
 
                         <span className="text-white group-hover:text-black">Criar Jogo</span>
 
@@ -24,9 +24,9 @@ export default function Navbar() {
 
                         
                         <button type="submit" 
-                            className="group bg-black px-4 py-2 rounded hover:bg-white border-2 border-black hover:border-black transition flex items-center gap-2">
+                            className="group bg-black px-4 py-2 rounded hover:bg-white border-2 border-black hover:border-black transition flex items-center">
                             
-                            <span className="text-white group-hover:text-black">Entrar em Partida</span>
+                            <span className="text-white group-hover:text-black">Entrar Partida</span>
                             
                         </button>
 
@@ -36,7 +36,7 @@ export default function Navbar() {
                             id="gameId"
                             onChange={(e) => setGameId(e.target.value)}
                             placeholder="Insira o ID da Partida"
-                            className='bg-white border-2 rounded border-black px-4'
+                            className='bg-white border-2 rounded border-black px-4 '
                         />
                         
                     </form>
