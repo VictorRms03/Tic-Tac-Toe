14960192a0db578a551f40f4b9e6915c70a5cfd8
Victor Ramos <victorrms03@gmail.com>
2025-07-25 03:35:22 -0300

    feat: logo do jogo agora é um link

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { createGame, joinGame } from "@/utils/gameLogic";
 import { useState } from 'react';
+import Link from 'next/link';
 
 /**
  * Gera a seção de navegação do jogo Tic Tac Toe: Online
@@ -14,8 +15,10 @@ export default function Navbar() {
         <header className="w-full px-2 sm:px-6 py-4 shadow-lg top-0 z-50">
             <div className="sm:max-w-11/12 xl:max-w-9/12 mx-auto flex items-center justify-between">
 
-                <h1 className='hidden sm:inline text-xl sm:text-2xl lg:text-4xl'>Tic-Tac-Toe: Online</h1>
-        
+                <Link href="https://tic-tac-toe-online-victorrms.vercel.app/" target="_blank" className="hidden sm:inline text-xl sm:text-2xl lg:text-4xl">
+                    <h1 className=''>Tic-Tac-Toe: Online</h1>
+                </Link>
+
                 <div className='flex space-x-2 sm:space-x-6 lg:space-x-10'>
 
                     <button onClick={createGame} className="group bg-black px-4 py-2 rounded hover:bg-white border-2 border-black hover:border-black transition flex items-center">
@@ -44,8 +47,6 @@ export default function Navbar() {
                         />
                         
                     </form>
-
-                    
                     
                 </div>
 
